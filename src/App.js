04ac6fad6951fc80519db7a34db6443eaf237c65
Flavio <fdipietro01@gmail.com>
2023-04-01 01:33:02 -0300

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,108 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import { images, deathMedal, winnerMedal } from "./images/index";
 
 function App() {
+  const parlaments = [
+    {
+      text: "Bienvenida al juego de la pista oculta: Una de nuestras mascotas, a cambio de su comida favorita, va a darte la pista que necesitas para encontrar su regalo.",
+    },
+    {
+      text: "Pero cuidado, no todas nuestras mascotas toleran esa comida. Podrías envenenar a alguna. Tenés 3 oportunidades. Suerte!",
+    },
+    {
+      text: "(Tenés que tocar en cada mascota para alimentarlo)",
+    },
+  ];
+  const [pets, setPets] = useState(images);
+  const [textCount, setTexttextCount] = useState(0);
+  const [showImages, setShowImage] = useState(false);
+  const [winnerNumber, setWinnerNumber] = useState();
+  const [winnerExist, setWinnerExist] = useState(false);
+  const [comments, setComments] = useState([]);
+
+  const reload = () => {
+    window.location.reload(true);
+  };
+  const handleKill = (idx) => {
+    if (idx !== winnerNumber && !winnerExist) {
+      setComments([...comments, pets[idx].text]);
+      pets.splice(idx, 1, deathMedal);
+      console.log(pets);
+      setPets([...pets]);
+    } else {
+      setPets([winnerMedal]);
+      setComments([winnerMedal.text]);
+      setWinnerExist(true);
+    }
+  };
+  const handleShow = () => setShowImage(true);
+  const handlebutton = () => {
+    setTexttextCount(textCount + 1);
+  };
+
+  useEffect(() => {
+    const winnerIdx = Math.floor(Math.random() * pets.length);
+    setWinnerNumber(winnerIdx);
+  }, []);
   return (
     <div className="App">
+      <p className="Tittle"> FELIZ CUMPLEAÑITOS</p>
+      <p className="Tittle"> MARÍA DELFINA DOLORES</p>
+
+      <div className="TextContainer">
+        {parlaments.map(({ text }, idx) => {
+          if (idx <= textCount) {
+            return (
+              <div class="text-typing">
+                <div className="Text text-typing-son">{text}</div>
+              </div>
+            );
+          }
+        })}
+      </div>
+      <div className="btnContainer">
+        {textCount !== parlaments.length ? (
+          <button className="button" onClick={handlebutton}>
+            Continuar{" "}
+          </button>
+        ) : (
+          <button className="button btn2" onClick={handleShow}>
+            A Jugar
+          </button>
+        )}
+      </div>
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="coinBox">
+          {showImages &&
+            pets.map((pet, idx) => (
+              <img
+                src={pet.url}
+                className="App-logo"
+                alt="logo"
+                onClick={() => comments.length !== 3 && handleKill(idx)}
+              />
+            ))}
+        </div>
+        {comments.length === 3 && (
+          <div className="endGame">
+            <p className="Tittle">Juego Terminado</p>
+            <button onClick={reload} className="button btn2 btn3">
+              Volver a Jugar
+            </button>
+          </div>
+        )}
       </header>
+      <div className="TextContainer SpeedText">
+        {comments.map((text, idx) => (
+          <div class="text-typing">
+            <div className="Text text-typing-son">
+              Intento n° {idx + 1}: {text}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/images/index.js
@@ -0,0 +1,43 @@
+import cato from "./cato.jpeg";
+import flop from "./flop.jpg";
+import nali from "./nali.JPG";
+import sonic from "./sonic.jpg";
+import ares from "./ares.jpg";
+import bone from "./bone.jpeg";
+import clue from "./pista.jpg";
+
+export const images = [
+  {
+    name: "Simón",
+    text: "Cato, Simón, Budín, Tutisín-Con-Queso, Bebi, ¿qué nombre va en la lápida?",
+    url: cato,
+  },
+  {
+    name: "Nala",
+    text: "Cuanta maldad... Nalu nunca le hizo mal a nadie. Merecés el infierno",
+    url: nali,
+  },
+  { name: "Sonic", text: "Sonic es inmortal, ya no puede morir", url: sonic },
+  {
+    name: "Flip-Flop",
+    text: "Bueno, no era el ser más pacífico pero no merecía esto. Pobre Flop",
+    url: flop,
+  },
+  {
+    name: "Ares",
+    text: "Acabás de matar a PapachuSaurio. ¡Que terrible madre!",
+    url: ares,
+  },
+];
+
+export const deathMedal = {
+  name: "Killed",
+  text: "Killed",
+  url: bone,
+};
+
+export const winnerMedal = {
+  name: "winner",
+  text: "Bueno, prestá mucha atención porque acá viene la siguiente pista: ...Bueno, no es una pista. Andá a buscar tu regalo al baúl del auto. ¿Qué te pensabas? ¿Que íbamos a estar toda la noche jugando?",
+  url: clue,
+};
